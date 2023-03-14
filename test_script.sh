a9ae8e76a94a273267df9798354cd7dbe10c0131
Siddharth Gupta <siddharth.gupta@st.ovgu.de>
2023-03-14 20:46:27 +0100

    changes made on test script

--- test_script.sh
+++ test_script.sh
@@ -9,8 +9,15 @@
 
 
 #Counting the number of logs through running the script and storing output in count variable
-count=$(bash student_solution.sh | wc -l)
+count_sol=$(bash student_solution.sh | wc -l)
 
+#Taking counts from the log file directly
+count_test=$(cat Linux_2k.log | grep '^Jun 18' |awk '{print $3}' | sed 's/://g' | awk '$1 >= 20000 && $1 <= 40000' | wc -l)
 
-# This looping statment check the variable counts with the exact count and if both match then solution is correct otherwise solution is wrong
-if [ $count == 16 ]; then echo "Solution is correct !"; else echo "Solution is not correct !"; fi
+# If the script count equals the count from the log file then solution is correct
+if [ $count_sol == $count_test ];
+then
+       	echo "Solution is correct !"; 
+else 
+	echo "Solution is not correct !"; 
+fi
