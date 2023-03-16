54fb23d0f0df8c488c6a230c60742e9bd2afb09c
Siddharth Gupta <siddharth.gupta@st.ovgu.de>
2023-03-16 16:28:16 +0100

    changes updated in solution script

--- student_solution.sh
+++ student_solution.sh
@@ -7,5 +7,14 @@
 #####################################################################
 
 
+count=$(cat Linux_2k.log | grep "^Jun 18" | awk -F'[: ]' '$3 >= 02 && $3 < 04 { print }' | wc -l)
+echo "***********************************************"
+echo "Number of logs fetched : $count"
+
+echo "***********************************************"
+
+
 # Below command takes the entire logs then filter the logs only for June 18 and then awk command takes the date field and compare the time between 2 - 4
 cat Linux_2k.log | grep "^Jun 18" | awk -F'[: ]' '$3 >= 02 && $3 < 04 { print }'
+echo "***********************************************"
+
